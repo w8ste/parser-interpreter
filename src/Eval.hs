4f87561d0bd1e9af0ac6886d60f8970032767a01
Hannes <hannes10042004@icloud.com>
2025-04-15 01:31:34 +0200

    Made it consistent

--- src/Eval.hs
+++ src/Eval.hs
@@ -8,4 +8,4 @@ eval (Add x y)   = eval x + eval y
 eval (Sub x y)   = eval x - eval y
 eval (Neg x y)   = - eval x
 eval (Mul x y)   = eval x * eval y
-eval (Div a b)   = eval a `div` eval b
+eval (Div x y)   = eval x `div` eval y
